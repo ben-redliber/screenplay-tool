0256a20daf1e9fc6ff88ad541a89bb4ce7f759a8
ben-redliber <90793036+benthewill@users.noreply.github.com>
2024-05-12 22:06:08 +0700

    Adjusted Drizzle Schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -10,25 +10,16 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `screenplay-tool_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const screenplays = createTable("screenplays", {
+  screenplay_id: serial("screenplay_id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+  created_at: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updated_at: timestamp("updated_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  user_id: varchar("user_id", { length: 256 }),
+});
